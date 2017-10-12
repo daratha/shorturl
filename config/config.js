338f5856ee32abc2029fb0fb22c5d138ef7293b6
Daratha <daratha@gmail.com>
2017-10-13 04:07:44 +0530

    change the base url to aws url

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 module.exports = {
   // base_url : 'http://localhost/',
-  base_url : 'ec2-52-14-29-158.us-east-2.compute.amazonaws.com',
+  base_url : 'ec2-52-14-29-158.us-east-2.compute.amazonaws.com/',
   server_port: '1337'
 };
