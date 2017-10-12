3774e9fab1863a7b6a7a8608f21e643cdffc3863
Daratha <daratha@gmail.com>
2017-10-13 04:02:46 +0530

    added mime type file to nginx, change the base url to aws url

--- config/config.js
+++ config/config.js
@@ -1,4 +1,5 @@
 module.exports = {
-  base_url : 'http://localhost/',
+  // base_url : 'http://localhost/',
+  base_url : 'ec2-52-14-29-158.us-east-2.compute.amazonaws.com',
   server_port: '1337'
 };
